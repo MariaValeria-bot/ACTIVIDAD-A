31e9da8c5fbcd355859376e92204a1b5a403de46
dam <dam@203PC07.intern>
2020-12-01 12:18:42 +0100

    Tercera version B

--- src/ejerciciogit/EjercicioGit.java
+++ src/ejerciciogit/EjercicioGit.java
@@ -17,7 +17,7 @@ public class EjercicioGit {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Segunda linea");
-        System.out.println("Tercera linea");
+        System.out.println("Tercera linea-B");
     }
     
 }
