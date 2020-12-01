02aa1042afba9b98238c6266a3952f48608a6c8f
dam <dam@172.26.46.238>
2020-12-01 12:02:29 +0100

    segunda versión

--- src/ejerciciogit/EjercicioGit.java
+++ src/ejerciciogit/EjercicioGit.java
@@ -16,6 +16,7 @@ public class EjercicioGit {
      */
     public static void main(String[] args) {
         System.out.println("Hola");
+        System.out.println("Segunda linea");
     }
     
 }
